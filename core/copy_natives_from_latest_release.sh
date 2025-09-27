78c7fc7a006a456c58787b985ee1a64775a8e73a
amsam0 <21845375-amsam0@users.noreply.gitlab.com>
2025-09-27 08:40:35 -0700

    Add script for copying natives from latest modrinth release

---
+++ core/copy_natives_from_latest_release.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+yellow="\033[0;33m"
+green="\033[0;32m"
+red="\033[0;31m"
+clear="\033[0m"
+
+
+# Download voicechat-discord
+file="src/main/resources/voicechat-discord.jar"
+if [ ! -f $file ]; then
+  url=$(curl -g -s "https://api.modrinth.com/v2/project/S1jG5YV5/version" | jq -r '.[0].files[0].url')
+
+  echo -e -n "${yellow}Downloading voicechat-discord from ${clear}$url${yellow}..."
+  curl -s -o $file $url
+  echo -e "downloaded${clear}"
+else
+  echo -e "${green}voicechat-discord already downloaded${clear}"
+fi
+
+cd src/main/resources
+
+echo -e "${yellow}Extracting natives${yellow}..."
+jar xf voicechat-discord.jar natives
+
+echo -e "${yellow}Removing old jar${yellow}..."
+rm -v voicechat-discord.jar
+
+echo -e "${green}Done!${clear}"
