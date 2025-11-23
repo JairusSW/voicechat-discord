e147e6dc15e343746acc3619e2cc0e97868e6694
amsam0 <21845375-amsam0@users.noreply.gitlab.com>
2025-11-22 19:23:36 -0800

    Remove unused getCraftWorld reflection helper

--- paper/src/main/java/dev/amsam0/voicechatdiscord/BukkitHelper.java
+++ paper/src/main/java/dev/amsam0/voicechatdiscord/BukkitHelper.java
@@ -7,10 +7,6 @@ public final class BukkitHelper {
         return Class.forName(Bukkit.getServer().getClass().getPackage().getName() + ".CraftServer");
     }
 
-    public static Class<?> getCraftWorld() throws ClassNotFoundException {
-        return Class.forName(Bukkit.getServer().getClass().getPackage().getName() + ".CraftWorld");
-    }
-
     public static Class<?> getVanillaCommandWrapper() throws ClassNotFoundException {
         return Class.forName(Bukkit.getServer().getClass().getPackage().getName() + ".command.VanillaCommandWrapper");
     }
